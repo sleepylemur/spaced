f77854393afd6cdf960b8251919017c7d45e89e6
Morgan <c5ipo7i@yahoo.com>
2022-05-02 20:55:28 -0700

    feat: added question_id

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import psycopg2
 from psycopg2 import pool
 import os
+
 # from algorithm import algo
 
 POSTGRESQL_URL = os.getenv("POSTGRESQL_URL")
@@ -9,15 +10,16 @@ pool = pool.SimpleConnectionPool(1, 20, POSTGRESQL_URL)
 
 app = Flask(__name__)
 
+
 def next_question():
     with pool.getconn() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("select question, answer from questions limit 1")
-            question, answer = cursor.fetchone()
-            return question, answer
+            cursor.execute("select question, id from questions limit 1")
+            question, question_id = cursor.fetchone()
+            return question, question_id
 
 
 @app.route("/")
 def hello_world():
-    question, answer = next_question()
-    return render_template("page.html", question=question)
+    question, question_id = next_question()
+    return render_template("page.html", question=question, question_id=question_id)
