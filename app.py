ef3517d4d741c9c4890649c4ed691deee6e7013d
Morgan <c5ipo7i@yahoo.com>
2022-05-02 20:29:55 -0700

    chore: renamed server -> app. added some folder paths

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 import psycopg2
 import os
-POSTGRESQL_URL = os.getenv('POSTGRESQL_URL')
+
+POSTGRESQL_URL = os.getenv("POSTGRESQL_URL")
 pool = psycopg2.pool.SimpleConnectionPool(1, 20, POSTGRESQL_URL)
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
@@ -13,4 +15,4 @@ def hello_world():
             query = cursor.execute("select * from questions")
             result = query.fetch_all()
             print(row for row in result)
-    return render_template('page.html', question="yoyo")
+    return render_template("page.html", question="yoyo")
