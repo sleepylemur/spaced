14a18d108bcf5ce4275dfb026f59b344f3651762
Morgan <c5ipo7i@yahoo.com>
2022-05-03 13:38:26 -0700

    chore:renamed app to main

--- main.py
+++ main.py
@@ -44,7 +44,46 @@ def next_question(conn):
     with conn.cursor() as cursor:
         cursor.execute(
             """
-            select question, id from questions limit 1
+            with next_questions as (
+                select id, question_id, lead(question_id) over (order by id) from history
+            ),
+            blocked as (
+                (
+                    -- block questions from happening in the same order as they were seen last time
+                    select lead as blocked_id from next_questions
+                    where
+                        question_id = (select question_id from history order by id desc limit 1)
+                        and lead is not null
+                    order by id desc
+                    limit 1
+                ) union (
+                    -- block the same question from being shown twice in a row
+                    select question_id as blocked_id
+                    from history
+                    order by id desc
+                    limit 1
+                )
+            ),
+            question_age as (
+                select question_id, min(row_number) as age from (
+                    select question_id, row_number() over (order by id desc)
+                    from history
+                ) as inner_query
+                where question_id != all (select blocked_id from blocked)
+                group by question_id
+            ),
+            unseen_questions as (
+                select questions.id
+                from questions
+                left join history on questions.id = history.question_id
+                where history.question_id is null
+            )
+            select question, id from questions where id =
+                case when exists(select 1 from unseen_questions) then
+                    (select id from unseen_questions limit 1)
+                else
+                    (select question_id from question_age order by age desc limit 1)
+                end;
         """
         )
         question, question_id = cursor.fetchone()
@@ -84,15 +123,16 @@ def get_answer(question_id):
     with pool.getconn() as conn:
         if request.method == "POST":
             user_answer = request.form["answer"]
-            new_question, next_id = next_question(conn)
             question_id = request.form["question_id"]
             answer = get_answer(conn, question_id)[0]
             result = answer == user_answer
             save_answer(conn, question_id, result)
+
+            new_question, next_id = next_question(conn)
             return render_template(
                 "page.html",
                 question=new_question,
-                question_id=question_id,
+                question_id=next_id,
                 answer=answer,
                 result=result,
             )
@@ -101,16 +141,6 @@ def get_answer(question_id):
             return render_template(
                 "page.html", question=question, question_id=question_id
             )
-
-
-def get_answer(conn, question_id):
-    with conn.cursor() as cursor:
-        cursor.execute(
-            "select answer from questions where id = %(question_id)s",
-            {"question_id": question_id},
-        )
-        answer = cursor.fetchone()
-        return answer
 
 
 def save_answer(conn, question_id, result):
