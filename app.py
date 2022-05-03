fd2ef07ea79fcc34f6904957effcebbac451da21
Evan Griffiths <griffithse@gmail.com>
2022-05-02 20:52:48 -0700

    Add next_answer

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ from flask import Flask, render_template
 import psycopg2
 from psycopg2 import pool
 import os
-from algorithm import algo
+# from algorithm import algo
 
 POSTGRESQL_URL = os.getenv("POSTGRESQL_URL")
-pool = pool.SimpleConnectionPool(1, 20, POSTGRESQL_URL, database="spaced")
+pool = pool.SimpleConnectionPool(1, 20, POSTGRESQL_URL)
 
 app = Flask(__name__)
+
+def next_question():
+    with pool.getconn() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("select question, answer from questions limit 1")
+            question, answer = cursor.fetchone()
+            return question, answer
 
 
 @app.route("/")
 def hello_world():
-    with pool.getconn() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("select * from questions")
-            result = cursor.fetchall()
-            print(row for row in result)
-    return render_template("page.html", question="a?")
+    question, answer = next_question()
+    return render_template("page.html", question=question)
